762e3d7d76f053a89c995952b730b17ed22b777a
张龙 <wb-zhanglong@sdc.icbc.com.cn>
2018-05-15 19:46:12 +0800

    AdRecyclerView

--- adrecyclerview/src/main/java/zlonglove/cn/adrecyclerview/adapter/MenuRecyclerListHeaderWrapper.java
+++ adrecyclerview/src/main/java/zlonglove/cn/adrecyclerview/adapter/MenuRecyclerListHeaderWrapper.java
@@ -2,6 +2,7 @@ package zlonglove.cn.adrecyclerview.adapter;
 
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -15,6 +16,7 @@ import zlonglove.cn.adrecyclerview.base.BaseHeaderFooterRecyclerAdapterWrapper;
 import zlonglove.cn.adrecyclerview.base.OnRecyclerItemClickListener;
 import zlonglove.cn.adrecyclerview.entity.EditItem;
 import zlonglove.cn.adrecyclerview.entity.MenuItem;
+import zlonglove.cn.adrecyclerview.helper.MenuHelper;
 import zlonglove.cn.adrecyclerview.viewHolder.MenuEditRecyclerListHolder;
 
 
@@ -25,13 +27,14 @@ import zlonglove.cn.adrecyclerview.viewHolder.MenuEditRecyclerListHolder;
  *
  * @version 1.0
  */
-public class MenuRecyclerListHeaderWrapper extends BaseHeaderFooterRecyclerAdapterWrapper<EditItem,EditItem,MenuEditRecyclerListHolder,MenuEditRecyclerListHolder> implements RecyclerViewDragDropManager.OnItemDragEventListener {
+public class MenuRecyclerListHeaderWrapper extends BaseHeaderFooterRecyclerAdapterWrapper<EditItem, EditItem, MenuEditRecyclerListHolder, MenuEditRecyclerListHolder> implements RecyclerViewDragDropManager.OnItemDragEventListener {
+    private final String TAG = MenuRecyclerListHeaderWrapper.class.getSimpleName();
     private OnRecyclerItemClickListener<MenuItem> mOnChildItemClickListener;
     private MenuHeaderRecyclerGridAdapter.OnDeleteClickListener mOnDeleteClickListener;
     private RecyclerViewDragDropManager mDragDropManager;
     private MenuHeaderRecyclerGridAdapter adapter;//原装适配器
     private RecyclerView.Adapter dragAdapter;//对原装适配器封装包裹后的实现了拖拽功能的适配器
-    private int itemMoveMode= RecyclerViewDragDropManager.ITEM_MOVE_MODE_DEFAULT;
+    private int itemMoveMode = RecyclerViewDragDropManager.ITEM_MOVE_MODE_DEFAULT;
     private boolean hasDragChanged;//优化刷新数据的参数，是否发生过拖拽
 
     public void setOnChildItemClickListener(OnRecyclerItemClickListener<MenuItem> onChildItemClickListener) {
@@ -53,7 +56,7 @@ public class MenuRecyclerListHeaderWrapper extends BaseHeaderFooterRecyclerAdapt
 
     @Override
     public MenuEditRecyclerListHolder createHeaderViewHolder(ViewGroup parent, int viewType) {
-        return new MenuEditRecyclerListHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.header_menu_edit_recycler,parent,false));
+        return new MenuEditRecyclerListHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.header_menu_edit_recycler, parent, false));
     }
 
     @Override
@@ -65,10 +68,10 @@ public class MenuRecyclerListHeaderWrapper extends BaseHeaderFooterRecyclerAdapt
     @Override
     public void bindHeaderViewHolder(MenuEditRecyclerListHolder headerViewHolder, EditItem headerItem) {
         headerViewHolder.tv_group_name.setText(headerItem.getGroup());
-        adapter=new MenuHeaderRecyclerGridAdapter(headerItem.getMenuItemList(),headerViewHolder.recyclerView,true);
+        adapter = new MenuHeaderRecyclerGridAdapter(headerItem.getMenuItemList(), headerViewHolder.recyclerView, true);
         adapter.setOnRecyclerItemClickListener(mOnChildItemClickListener);
         adapter.setOnDeleteClickListener(mOnDeleteClickListener);
-        mDragDropManager=new RecyclerViewDragDropManager();
+        mDragDropManager = new RecyclerViewDragDropManager();
         mDragDropManager.setItemMoveMode(itemMoveMode);
         // Start dragging after long press
         mDragDropManager.setInitiateOnLongPress(true);
@@ -81,10 +84,10 @@ public class MenuRecyclerListHeaderWrapper extends BaseHeaderFooterRecyclerAdapt
         //dragDropManager.setDraggingItemAlpha(0.8f);
         mDragDropManager.setDraggingItemScale(1.1f);
         //dragDropManager.setDraggingItemRotation(15.0f);
-        dragAdapter =mDragDropManager.createWrappedAdapter(adapter);
+        dragAdapter = mDragDropManager.createWrappedAdapter(adapter);
         headerViewHolder.recyclerView.setAdapter(dragAdapter);
-        headerViewHolder.recyclerView.setLayoutManager(new GridLayoutManager(headerViewHolder.recyclerView.getContext(),4));
-        GeneralItemAnimator itemAnimator=new DraggableItemAnimator();
+        headerViewHolder.recyclerView.setLayoutManager(new GridLayoutManager(headerViewHolder.recyclerView.getContext(), 4));
+        GeneralItemAnimator itemAnimator = new DraggableItemAnimator();
         headerViewHolder.recyclerView.setItemAnimator(itemAnimator);
 
         mDragDropManager.attachRecyclerView(headerViewHolder.recyclerView);//关键步骤，设置好DragDropManager和RecyclerView后将二者绑定实现拖拽功能
@@ -131,10 +134,11 @@ public class MenuRecyclerListHeaderWrapper extends BaseHeaderFooterRecyclerAdapt
 
     @Override
     public void onItemDragFinished(int fromPosition, int toPosition, boolean result) {
-        if(fromPosition!=toPosition && result){
-            if(adapter!=null){
-                //MenuHelper.savePreferFavoriteList(adapter.getRecyclerItems());
-                hasDragChanged=true;
+        if (fromPosition != toPosition && result) {
+            Log.i(TAG, "--->onItemDragFinished()" + fromPosition + "->" + toPosition);
+            if (adapter != null) {
+                MenuHelper.savePreferFavoriteList(adapter.getRecyclerItems());
+                hasDragChanged = true;
             }
         }
     }
@@ -147,19 +151,20 @@ public class MenuRecyclerListHeaderWrapper extends BaseHeaderFooterRecyclerAdapt
     /**
      * 外部页面销毁之前调用
      */
-    public void releaseDragManager(){
-        if(mDragDropManager!=null){
+    public void releaseDragManager() {
+        if (mDragDropManager != null) {
             mDragDropManager.release();
-            mDragDropManager=null;
+            mDragDropManager = null;
         }
-        if(dragAdapter !=null){
+        if (dragAdapter != null) {
             WrapperAdapterUtils.releaseAll(dragAdapter);
-            dragAdapter =null;
+            dragAdapter = null;
         }
     }
 
     /**
      * 是否发生过拖拽变化
+     *
      * @return
      */
     public boolean isHasDragChanged() {
@@ -169,23 +174,23 @@ public class MenuRecyclerListHeaderWrapper extends BaseHeaderFooterRecyclerAdapt
     /**
      * 通知刷新子列表数据
      */
-    public void notifyChildDataChanged(){
-        if(adapter!=null){
+    public void notifyChildDataChanged() {
+        if (adapter != null) {
             adapter.notifyDataSetChanged();
         }
     }
 
-    public void notifyChildDataAdded(MenuItem item){
-        if(adapter!=null){
-            if(!adapter.getRecyclerItems().contains(item)){
+    public void notifyChildDataAdded(MenuItem item) {
+        if (adapter != null) {
+            if (!adapter.getRecyclerItems().contains(item)) {
                 adapter.getRecyclerItems().add(item);
                 adapter.notifyDataSetChanged();
             }
         }
     }
 
-    public void notifyChildDataRemoved(MenuItem item){
-        if(adapter!=null){
+    public void notifyChildDataRemoved(MenuItem item) {
+        if (adapter != null) {
             adapter.getRecyclerItems().remove(item);
             adapter.notifyDataSetChanged();
         }
