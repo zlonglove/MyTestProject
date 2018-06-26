68ed0edde128a8da07595d98734b9fbd873e1441
张龙 <wb-zhanglong@sdc.icbc.com.cn>
2018-06-26 15:11:43 +0800

    Toobar

--- app/src/main/java/com/ISHello/TouchEvent/TouchEventActivity.java
+++ app/src/main/java/com/ISHello/TouchEvent/TouchEventActivity.java
@@ -1,10 +1,14 @@
 package com.ISHello.TouchEvent;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.ISHello.base.base.BaseActivity;
 import com.example.ishelloword.R;
@@ -12,6 +16,7 @@ import com.example.ishelloword.R;
 public class TouchEventActivity extends BaseActivity implements View.OnClickListener {
     private final String TAG = "TouchEventActivity";
     private MyTextView myTextView;
+    private Button touch_event_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +24,18 @@ public class TouchEventActivity extends BaseActivity implements View.OnClickList
         setContentView(R.layout.activity_touch_event);
         setTitle("触摸事件分发");
         myTextView = (MyTextView) findViewById(R.id.my_text_view);
-        //myTextView.setOnClickListener(this);
+        touch_event_button = (Button) findViewById(R.id.touch_event_button);
+        myTextView.setOnClickListener(this);
         ((ViewGroup) getWindow().getDecorView().findViewById(android.R.id.content)).getChildAt(0).setBackgroundColor(Color.GREEN);
     }
 
+    private Animator createTranslationAnimations(View view, float startX, float endX, float startY, float endY) {
+        ObjectAnimator animX = ObjectAnimator.ofFloat(view, "translationX", startX, endX);
+        ObjectAnimator animY = ObjectAnimator.ofFloat(view, "translationY", startY, endY);
+        AnimatorSet animSetXY = new AnimatorSet();
+        animSetXY.playTogether(animX, animY);
+        return animSetXY;
+    }
    /* @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         switch (ev.getAction()) {
@@ -64,6 +77,7 @@ public class TouchEventActivity extends BaseActivity implements View.OnClickList
         switch (v.getId()) {
             case R.id.my_text_view:
                 Log.i(TAG, "--->MyTextView onClick");
+                createTranslationAnimations(touch_event_button,-touch_event_button.getWidth(),0,0,0).start();
                 break;
         }
     }
