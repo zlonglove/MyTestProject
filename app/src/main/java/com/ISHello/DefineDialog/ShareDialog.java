e80274e2a9b6b8d0d954bd4ddcf77f6c600c4618
张龙 <wb-zhanglong@sdc.icbc.com.cn>
2018-11-26 16:59:40 +0800

    sharedialog

--- app/src/main/java/com/ISHello/DefineDialog/ShareDialog.java
+++ app/src/main/java/com/ISHello/DefineDialog/ShareDialog.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.example.ishelloword.R;
 
