89e3cd52ceb07af0bbe7e4eadbbcd225330dfffc
张龙 <wb-zhanglong@sdc.icbc.com.cn>
2019-05-27 14:41:19 +0800

    fragment

--- tabswitch/src/main/java/zlonglove/cn/tabswitch/ui/NewsFragment.java
+++ tabswitch/src/main/java/zlonglove/cn/tabswitch/ui/NewsFragment.java
@@ -36,7 +36,7 @@ public class NewsFragment extends BaseFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "--->onCreate");
+        Log.d(TAG, "--->onCreate()");
     }
 
     @Override
@@ -50,48 +50,48 @@ public class NewsFragment extends BaseFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Log.d(TAG, "--->onActivityCreated");
+        Log.d(TAG, "--->onActivityCreated()");
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        Log.d(TAG, "--->onStart");
+        Log.d(TAG, "--->onStart()");
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Log.d(TAG, "--->onResume");
+        Log.d(TAG, "--->onResume()");
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.d(TAG, "--->onPause");
+        Log.d(TAG, "--->onPause()");
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Log.d(TAG, "--->onStop");
+        Log.d(TAG, "--->onStop()");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        Log.d(TAG, "--->onDestroyView");
+        Log.d(TAG, "--->onDestroyView()");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "--->onDestroy");
+        Log.d(TAG, "--->onDestroy()");
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        Log.d(TAG, "--->onDetach");
+        Log.d(TAG, "--->onDetach()");
     }
 }
