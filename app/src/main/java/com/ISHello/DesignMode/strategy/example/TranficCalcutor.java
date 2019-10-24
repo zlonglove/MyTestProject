d69abe534cbe69ddbdfa882503e4fb7964506ee2
zhanglong <zhanglongit@126.com>
2019-10-24 21:18:25 +0800

    策略模式

--- app/src/main/java/com/ISHello/DesignMode/strategy/example/TranficCalcutor.java
+++ app/src/main/java/com/ISHello/DesignMode/strategy/example/TranficCalcutor.java
@@ -1,16 +1,7 @@
 package com.ISHello.DesignMode.strategy.example;
 
 public class TranficCalcutor {
-    CalculateStrategy calculateStrategy;
-
-    public static void main(String[] args) {
-        TranficCalcutor tranficCalcutor = new TranficCalcutor();
-        tranficCalcutor.setStrategy(new BusStrategy());
-        System.out.println("公交车计价为:" + tranficCalcutor.calculatePrice(16));
-
-        tranficCalcutor.setStrategy(new SubwayStrategy());
-        System.out.println("地铁计价为:" + tranficCalcutor.calculatePrice(16));
-    }
+    private CalculateStrategy calculateStrategy;
 
     public void setStrategy(CalculateStrategy calculateStrategy) {
         this.calculateStrategy = calculateStrategy;
---
+++ app/src/main/java/com/ISHello/DesignMode/strategy/example/TranficTest.java
@@ -0,0 +1,12 @@
+package com.ISHello.DesignMode.strategy.example;
+
+public class TranficTest {
+    public static void main(String[] args) {
+        TranficCalcutor mTranficCalcutor = new TranficCalcutor();
+        mTranficCalcutor.setStrategy(new BusStrategy());
+        System.out.println("公交车计价为:" + mTranficCalcutor.calculatePrice(16));
+
+        mTranficCalcutor.setStrategy(new SubwayStrategy());
+        System.out.println("地铁计价为:" + mTranficCalcutor.calculatePrice(16));
+    }
+}
